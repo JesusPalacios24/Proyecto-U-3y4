9048056c9a6e7259f32b52552b416bce15e56c7d
Martin Humberto Lopez Chavez <mhlch.22@gmail.com>
2024-11-10 20:11:25 -0600

    se hizo un desmadre, pero se hizo

--- pages/api/web_socket/web_socket/urls.py
+++ pages/api/web_socket/web_socket/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for microservicio project.
+URL configuration for web_socket project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
